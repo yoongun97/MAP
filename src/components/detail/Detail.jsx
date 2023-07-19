728838d6c659b6c41dd3def92fe7b34c3b6965f5
hgyeom <rrune3@naver.com>
2023-07-19 15:27:40 +0900

    카카오 맵 초기 데이터 마커 모두 보이기(#14)

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,45 +2,10 @@ import React from 'react';
 import * as S from './StyledDetail';
 import noImage from '../../assets/noimage.png';
 import KakaoMap from './KakaoMap';
-import { onClickSpotCreateMarker, onLoadKakaoMap } from '../../api/kakao';
+import { onClickSpotCreateMarker, onLoadKakaoMap, allMarkers } from '../../api/kakao';
+import { items } from '../../constant/items';
 const Detail = () => {
-  // 추후 tourAPI 연동시 변경 - 삭제
-  const items = [
-    {
-      addr1: '제주특별자치도 제주시 애월읍 평화로 2144',
-      addr2: '(애월읍)',
-      areacode: '39',
-      contentid: '637398',
-      firstimage: 'http://tong.visitkorea.or.kr/cms/resource/27/1950527_image2_1.jpg',
-      firstimage2: 'http://tong.visitkorea.or.kr/cms/resource/27/1950527_image3_1.jpg',
-      mapx: '126.3980250961',
-      mapy: '33.4042417055',
-      title: '말 테마파크 골프장 (렛츠런파크 제주)'
-    },
-    {
-      addr1: '제주특별자치도 제주시 애월읍 하소로 684-25',
-      addr2: '',
-      areacode: '39',
-      contentid: '2743787',
-      firstimage: 'http://tong.visitkorea.or.kr/cms/resource/32/2744732_image2_1.JPG',
-      firstimage2: 'http://tong.visitkorea.or.kr/cms/resource/32/2744732_image2_1.JPG',
-      mapx: '126.3937028464',
-      mapy: '33.4221618770',
-      title: '유수암 캠핑장'
-    },
-    {
-      addr1: '제주특별자치도 제주시 애월읍 녹고메길 152-1',
-      addr2: '(애월읍)',
-      areacode: '39',
-      contentid: '1755101',
-      firstimage: '',
-      firstimage2: '',
-      mapx: '126.4061271508',
-      mapy: '33.4050889621',
-      title: '제주승마공원'
-    }
-  ];
-  // 추후 파일을 분리 할 예정. script를 만들어 kakao api를 받아온다.
+  // script를 만들어 kakao api를 받아온다.
   const mapScript = document.createElement('script');
 
   mapScript.defer = true;
@@ -55,11 +20,15 @@ const Detail = () => {
     <S.detailContainer>
       <S.detailBox>상세 페이지</S.detailBox>
       <KakaoMap />
+      <button onClick={() => allMarkers()}>초기화</button>
       <S.detailPlaceList>
         <S.spotList>
           {items.map((item) => {
             return (
-              <S.spotCard key={item.contentid} onClick={() => onClickSpotCreateMarker(item.mapy, item.mapx)}>
+              <S.spotCard
+                key={item.contentid}
+                onClick={() => onClickSpotCreateMarker(item.mapy, item.mapx, item.title)}
+              >
                 <S.spotImage>
                   {item.firstimage ? (
                     <img src={item.firstimage} alt="명소 이미지" />
