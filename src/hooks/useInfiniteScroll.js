54987b671d1fcf9054093fedc1c28afcf894494b
abcd0978 <kimmin36944@gmail.com>
2023-07-20 01:54:59 +0900

    상세페이지 리스트

---
+++ src/hooks/useInfiniteScroll.js
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react';
+export default function useInfiniteScoll(callback) {
+  const observer = useRef(
+    new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            console.log('인터섹트');
+            callback();
+          }
+        });
+      },
+      { threshold: 1 }
+    )
+  );
+
+  const observe = (element) => {
+    console.log('훅:관찰 유지');
+    observer.current.observe(element);
+  };
+
+  const unobserve = (element) => {
+    console.log('훅: 관찰 풀음');
+    observer.current.unobserve(element);
+  };
+
+  return [observe, unobserve];
+}
